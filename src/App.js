5c9f42aadca871470133b640f305b08a14679412
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-10-29 12:43:41 +1100

    add game history functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function Command({ commands }) {
-  const { command, verified } = commands;
-  const sectionHeading = <h1 data-testid="command">Command History</h1>;
-  const text = verified ? sectionHeading : "!error" + sectionHeading;
-  return sectionHeading;
+function Command({ command }) {
+  const { input, verified } = command;
+
+  const text = verified ? input : `!error ${input}`;
+  console.log("text", text);
+  console.log("verified", verified);
+  const commandItem = `<p data-testid="command">${text}</p>`;
+
+  return commandItem;
 }
 
 function App() {
   const commands = [
-    { command: "PLACE 0,0,NORTH", verified: true },
-    { command: "PLACE 1,2,EAST", verified: true },
-    { command: "PLACE 10,0,SOUTH", verified: false },
-    { command: "LEFT", verified: true },
-    { command: "REPORT", verified: true },
-    { command: "PLACE 0,0,NOdRTH", verified: false },
-    { command: "PLACE 0,3,east", verified: false },
-    { command: "MOVE", verified: true },
-    { command: "MOdsfnklsVE", verified: false },
-    { command: "RIGHT", verified: true },
+    { input: "PLACE 0,0,NORTH", verified: true },
+    { input: "PLACE 1,2,EAST", verified: true },
+    { input: "PLACE 10,0,SOUTH", verified: false },
+    { input: "LEFT", verified: true },
+    { input: "REPORT", verified: true },
+    { input: "PLACE 0,0,NOdRTH", verified: false },
+    { input: "PLACE 0,3,east", verified: false },
+    { input: "MOVE", verified: true },
+    { input: "MOdsfnklsVE", verified: false },
+    { input: "RIGHT", verified: true },
   ];
+
   return (
     <div className="App">
-      <Command commands={commands} />
+      {commands.map((command, index) => {
+        return <Command command={command} key={index} />;
+      })}
     </div>
   );
 }
