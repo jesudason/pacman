07cef72c7661e358d6d75a6d5ef28215e4341b71
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-10-29 12:11:53 +1100

    setup react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Command({}) {
+  const sectionHeading = <h1 data-testid="command">Command History</h1>;
+
+  return sectionHeading;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Command />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, cleanup } from "@testing-library/react";
+import Command from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(<Command />);
+  const commandElement = screen.getByTestId("command");
+  expect(commandElement).toBeInTheDocument();
+  expect(commandElement).toHaveTextContent("Command History");
 });
