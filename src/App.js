faa6a48e9635f0dc929b727f9c9b8e14c78db69e
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-10-29 12:22:30 +1100

    add dummy data to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function Command({}) {
+function Command({ commands }) {
+  const { command, verified } = commands;
   const sectionHeading = <h1 data-testid="command">Command History</h1>;
-
+  const text = verified ? sectionHeading : "!error" + sectionHeading;
   return sectionHeading;
 }
 
 function App() {
+  const commands = [
+    { command: "PLACE 0,0,NORTH", verified: true },
+    { command: "PLACE 1,2,EAST", verified: true },
+    { command: "PLACE 10,0,SOUTH", verified: false },
+    { command: "LEFT", verified: true },
+    { command: "REPORT", verified: true },
+    { command: "PLACE 0,0,NOdRTH", verified: false },
+    { command: "PLACE 0,3,east", verified: false },
+    { command: "MOVE", verified: true },
+    { command: "MOdsfnklsVE", verified: false },
+    { command: "RIGHT", verified: true },
+  ];
   return (
     <div className="App">
-      <Command />
+      <Command commands={commands} />
     </div>
   );
 }
